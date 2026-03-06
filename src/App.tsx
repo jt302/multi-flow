029484e9f447bbedb65746fbd9180b0066e26934
Abel206321 <abel206321@gmail.com>
2026-03-06 18:48:12 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import { invoke } from '@tauri-apps/api/core';
+
+function App() {
+	const [greetMsg, setGreetMsg] = useState('');
+	const [name, setName] = useState('');
+
+	async function greet() {
+		// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+		setGreetMsg(await invoke('greet', { name }));
+	}
+
+	return (
+		<main className="container">
+			<h1>Welcome to Tauri + React</h1>
+		</main>
+	);
+}
+
+export default App;
